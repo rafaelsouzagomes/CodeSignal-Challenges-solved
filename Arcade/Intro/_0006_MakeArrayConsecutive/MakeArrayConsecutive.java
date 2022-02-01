66bc77770a3cabcd679abc57d8621b73fa51d002
rafaelsouzagomes <rafaelsouzagomes13@gmail.com>
2022-02-01 17:59:11 -0300

    exe 6

--- Arcade/Intro/_0006_MakeArrayConsecutive/MakeArrayConsecutive.java
+++ Arcade/Intro/_0006_MakeArrayConsecutive/MakeArrayConsecutive.java
@@ -12,4 +12,12 @@ public class MakeArrayConsecutive {
 				.map(i -> statues[i + 1] - statues[i] - 1)
 				.sum();
 	}
+	
+	public int solution2(int[] statues) {
+		Arrays.sort(statues);
+		return statues[statues.length-1] - statues[0] - statues.length +1; 
+	}
+	
+	
+	
 }
